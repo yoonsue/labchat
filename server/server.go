c0ea0c34a9f1d6754ad839bcf68192d9538dcb90
chanyoung <park910113@gmail.com>
2018-05-19 01:35:53 +0900

    server: add basic structure

---
+++ server/server.go
@@ -0,0 +1,18 @@
+package server
+
+// Server provides http service for the labchat service.
+type Server struct {
+	// TODO: implementation.
+}
+
+// NewServer creates a new labchat server with the given configuration.
+func NewServer(cfg *Config) (srv *Server, err error) {
+	// TODO: implementation.
+	return nil, nil
+}
+
+// Start runs the server and starts handling for incoming requests. All the
+// long-running server functionality should be implemented in goroutines.
+func (s *Server) Start() {
+	// TODO: implementation.
+}
