0e7930f4d5443d49265eb2af6320f17d667002b3
chanyoung <park910113@gmail.com>
2018-05-25 03:19:22 +0900

    server: add address field in config

--- server/config.go
+++ server/config.go
@@ -3,9 +3,12 @@ package server
 // Config is the value holder of the configurations of labchat server.
 type Config struct {
 	// TODO: implementation.
+	Address string
 }
 
 // DefaultConfig returns the default setting of the labchat configuration.
 func DefaultConfig() *Config {
-	return &Config{}
+	return &Config{
+		Address: "localhost:4748",
+	}
 }
