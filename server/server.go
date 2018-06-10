a853b3a60501d12eb3421a246576fdaee7756042
yoonsue <suejoe1996@gmail.com>
2018-06-10 13:40:39 +0900

    server: add handlehttp request /friend

--- server/server.go
+++ server/server.go
@@ -38,10 +38,6 @@ func (s *Server) Start() {
 	// }
 	// log.Println("Success: loadMsg")
 
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		res.Write([]byte("Hello, world!"))
-	})
-
 	http.HandleFunc("/labchat/", handlehttp)
 
 	// TODO: need to halt goroutine when the program is stopped.
@@ -79,12 +75,17 @@ type user struct {
 
 func handlehttp(w http.ResponseWriter, r *http.Request) {
 	log.Printf("received: %s\t %s\n", r.Method, html.EscapeString(r.URL.Path))
-
+	w.Write([]byte("Hello, world!"))
 	// curl -XGET 'https://:your_server_url/keyboard'
 	if r.Method == "GET" && r.URL.Path == "/labchat/keyboard" {
-		initkeyboard := make([]keyboard, 1)
-		initkeyboard[0].Type = "buttons"
-		initkeyboard[0].Buttons = []string{"option1", "option2", "option3"}
+		// CASE1 Type: 'text'
+		initkeyboard := keyboard{Type: "text"}
+
+		// // CASE2 Type: 'buttons'
+		// initkeyboard := make([]keyboard, 1)
+		// initkeyboard[0].Type = "buttons"
+		// initkeyboard[0].Buttons = []string{"option1", "option2", "option3"}
+
 		resp, err := json.Marshal(initkeyboard)
 		if err != nil {
 			log.Println(errors.Wrap(err, "failed to marshal 'keyboard'"))
@@ -139,6 +140,18 @@ func handlehttp(w http.ResponseWriter, r *http.Request) {
 
 	// curl -XPOST 'https://:your_server_url/friend' -d '{"user_key" : "HASHED_USER_KEY" }'
 	if r.Method == "POST" && r.URL.Path == "/labchat/friend" {
+		body, err := ioutil.ReadAll(r.Body)
+		r.Body.Close()
+		if err != nil {
+			log.Println(errors.Wrap(err, "failed to read body of /friend"))
+		}
+		log.Printf("body: %s\n", string(body))
+
+		var usr user
+		if err := json.Unmarshal(body, &usr); err != nil {
+			log.Println(errors.Wrap(err, "failed to unmarshal body of /friend"))
+		}
+		log.Printf("Friend %s joined\n", usr.UserKey)
 		return
 	}
 
@@ -161,6 +174,7 @@ func handlehttp(w http.ResponseWriter, r *http.Request) {
 var messageKeyMap = map[string]string{
 	"hi":    "hello",
 	"hello": "hi",
+	"name":  "LABchat",
 }
 
 func messageKey(rawmessage string) string {
@@ -181,5 +195,3 @@ func msgFor(tokens []string) string {
 	}
 	return strings.Join(tokens, " ") + "....????"
 }
-
-// Now I'm struggling with server setting....
