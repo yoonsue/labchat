d39e47833c31b362d7ea6fe0cf116b6367e2da3a
yoonsue <suejoe1996@gmail.com>
2018-06-11 20:04:21 +0900

    server: working on server_test

--- server/server_test.go
+++ server/server_test.go
@@ -1,7 +1,25 @@
 package server
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+
+	"github.com/yoonsue/labchat/server"
+)
 
 func TestServer(t *testing.T) {
 	// TODO: implementation.
+	serverConfig := server.DefaultConfig()
+	testserver, err := &Server{
+		cfg: serverConfig,
+	}, nil
+
+	testserver.Start()
+
+	resp, err := http.Get("http://" + testserver + "/labchat/keyboard")
+	if err != nil {
+		// handle err
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
 }
