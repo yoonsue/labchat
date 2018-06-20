fea7ad281ee9dfbedcbe5c7ff501bbe5a45ed3fa
yoonsue <suejoe1996@gmail.com>
2018-06-20 21:56:07 +0900

    repository: create repository package

---
+++ repository/config.go
@@ -0,0 +1,12 @@
+package repository
+
+// dbConfig is the value holder of the configurations of labchat database.
+type dbConfig struct {
+	// TODO: implementation.
+
+}
+
+// DefaultConfig returns the default setting of the labchat configuration.
+func DefaultConfig() *dbConfig {
+	return &dbConfig{}
+}
