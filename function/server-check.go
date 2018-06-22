7c62b538d495a122bdc5cbb7d0044db9596af9fe
yoonsue <suejoe1996@gmail.com>
2018-06-22 17:50:52 +0900

    function: add server check

---
+++ function/server-check.go
@@ -0,0 +1,10 @@
+package function
+
+import "github.com/yoonsue/labchat/model"
+
+func ServerCheck() model.ServerStatus {
+	status := model.ServerStatus{}
+	// Access somewhere to get status..
+
+	return status
+}
