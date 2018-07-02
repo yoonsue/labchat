bb066a058c7146ed34bd68a7ffed594871762073
yoonsue <suejoe1996@gmail.com>
2018-07-02 23:29:21 +0900

    repository: fix build error

--- repository/config.go
+++ repository/config.go
@@ -1,14 +1,14 @@
 package repository
 
-// dbConfig is the value holder of the configurations of labchat database.
-type dbConfig struct {
+// DBConfig is the value holder of the configurations of labchat database.
+type DBConfig struct {
 	// TODO: implementation.
 	env string
 }
 
 // DefaultConfig returns the default setting of the labchat configuration.
-func DefaultConfig() *dbConfig {
-	return &dbConfig{
+func DefaultConfig() *DBConfig {
+	return &DBConfig{
 		env: "none",
 	}
 }
