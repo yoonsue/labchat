7fc8236ae49b0b9eb84c351cf2df49d3a31df1d9
yoonsue <suejoe1996@gmail.com>
2018-07-20 22:33:35 +0900

    function: add 'scrapMenu' testcase

---
+++ function/school-menu_test.go
@@ -0,0 +1,33 @@
+package function
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestScrapMenu(t *testing.T) {
+	testCases := []struct {
+		url      string
+		expected string
+	}{
+		{
+			"http://www.hanyang.ac.kr/web/www/-254",
+			"[중식A]",
+		},
+		{
+			"http://www.hanyang.ac.kr/web/www/-255",
+			"[특식]",
+		},
+		{
+			"http://www.hanyang.ac.kr/web/www/-258",
+			"[한식]",
+		},
+	}
+
+	for _, c := range testCases {
+		result := scrapMenu(c.url)
+		if !strings.HasPrefix(result, c.expected) {
+			t.Errorf("start with '%s' on url %s, got '%s'", c.expected, c.url, result)
+		}
+	}
+}
