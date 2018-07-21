43f0bb8f517603c9a492b4b66dc21d776d16a502
yoonsue <suejoe1996@gmail.com>
2018-07-21 15:27:54 +0900

    server: add 'DefaultConfig' testcase

--- server/config_test.go
+++ server/config_test.go
@@ -1,7 +1,19 @@
 package server
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestConfig(t *testing.T) {
+func TestDefaultConfig(t *testing.T) {
 	// TODO: implementation.
+	// how to collarborate the fixed default address with config.go
+	config := &Config{
+		Address: "localhost:4748",
+	}
+
+	cfg := DefaultConfig()
+
+	if cfg.Address != config.Address {
+		t.Errorf("expected %s, got %s", config.Address, cfg.Address)
+	}
 }
