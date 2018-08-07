0ba83dc8f009e0b902194a115b974ffe25317bbb
yoonsue <suejoe1996@gmail.com>
2018-08-07 23:48:50 +0900

    repository: refactor in memory database

--- repository/inmem/inmem.go
+++ repository/inmem/inmem.go
@@ -1,26 +1,25 @@
 package inmem
 
 import (
-	"fmt"
 	"sync"
 
 	"github.com/yoonsue/labchat/model/menu"
 )
 
-// MenuRepository struct definition
+// MenuRepository struct definition.
 type MenuRepository struct {
 	mtx     sync.RWMutex
 	menuMap map[menu.Restaurant]menu.TodayMenu
 }
 
-// NewMenuRepository does several services according to Go Map
+// NewMenuRepository return a new instance of in-memory menu repository.
 func NewMenuRepository() menu.Repository {
 	return &MenuRepository{
 		menuMap: make(map[menu.Restaurant]menu.TodayMenu),
 	}
 }
 
-// Store ..
+// Store saves menu model in memory.
 func (r *MenuRepository) Store(target menu.Menu) error {
 	r.mtx.Lock()
 	defer r.mtx.Unlock()
@@ -28,49 +27,50 @@ func (r *MenuRepository) Store(target menu.Menu) error {
 	return nil
 }
 
-// Find ..
+// Find returns today's menus that match with the given restaurant.
 func (r *MenuRepository) Find(rest menu.Restaurant) (menu.TodayMenu, error) {
+	r.mtx.RLock()
+	defer r.mtx.RUnlock()
 	if val, exists := r.menuMap[rest]; exists {
 		return val, nil
 	}
 	return "none", nil
 }
 
-// menuURLMap
-var menuURLMap = map[string]string{
-	"교직원식당":   "http://www.hanyang.ac.kr/web/www/-254",
-	"학생식당":    "http://www.hanyang.ac.kr/web/www/-255",
-	"창업보육센터":  "http://www.hanyang.ac.kr/web/www/-258",
-	"창의인재원식당": "http://www.hanyang.ac.kr/web/www/-256",
-}
-
-var menuMap = map[string]string{
-	"창의인재원":  "",
-	"학생식당":   "",
-	"교직원식당":  "",
-	"창업보육센터": "",
-}
-
-func menuSave(title string, menu string) {
-	menuMap[title] = menu
-}
-
-func menuRead(title string) string {
-	val, exists := menuMap[title]
-	if !exists {
-		println("No '", title, "' exists")
+// FindAll returns all menus that were stored in memory.
+func (r *MenuRepository) FindAll() map[menu.Restaurant]menu.TodayMenu {
+	r.mtx.RLock()
+	defer r.mtx.RUnlock()
+	c := make(map[menu.Restaurant]menu.TodayMenu)
+	for key, val := range r.menuMap {
+		c[key] = val
 	}
-	return val
+	return c
 }
 
-func allMenu() string {
-	str := ""
-	for key := range menuMap {
-		str += key
-		str += "\t"
-		str += menuMap[key]
-		str += "\n"
-		fmt.Printf("key: %s\tvalue: %s", key, menuMap[key])
-	}
-	return str
-}
+// // menuURLMap
+// var menuURLMap = map[string]string{
+// 	"교직원식당":   "http://www.hanyang.ac.kr/web/www/-254",
+// 	"학생식당":    "http://www.hanyang.ac.kr/web/www/-255",
+// 	"창업보육센터":  "http://www.hanyang.ac.kr/web/www/-258",
+// 	"창의인재원식당": "http://www.hanyang.ac.kr/web/www/-256",
+// }
+
+// var menuMap = map[string]string{
+// 	"창의인재원":  "",
+// 	"학생식당":   "",
+// 	"교직원식당":  "",
+// 	"창업보육센터": "",
+// }
+
+// func allMenu() string {
+// 	str := ""
+// 	for key := range menuMap {
+// 		str += key
+// 		str += "\t"
+// 		str += menuMap[key]
+// 		str += "\n"
+// 		fmt.Printf("key: %s\tvalue: %s", key, menuMap[key])
+// 	}
+// 	return str
+// }
