010448db88c45dd1fe83f3f4d65940be732f4eac
yoonsue <suejoe1996@gmail.com>
2018-08-18 23:02:00 +0900

    server: split http handler using mux and move handling.go into server.go

--- server/server.go
+++ server/server.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/gorilla/mux"
+
 	"github.com/pkg/errors"
 	"github.com/yoonsue/labchat/function/menu"
 	"github.com/yoonsue/labchat/function/status"
@@ -77,95 +79,140 @@ type user struct {
 func (s *Server) handleHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("received: %s\t %s\n", r.Method, html.EscapeString(r.URL.Path))
 
-	// curl -XGET 'https://:your_server_url/keyboard'
-	if r.Method == "GET" && r.URL.Path == "/keyboard" {
-		// CASE1 Type: 'text'
-		initkeyboard := keyboard{Type: "text"}
+	rou := mux.NewRouter()
 
-		// // CASE2 Type: 'buttons'
-		// initkeyboard := make([]keyboard, 1)
-		// initkeyboard[0].Type = "buttons"
-		// initkeyboard[0].Buttons = []string{"option1", "option2", "option3"}
+	rou.HandleFunc("/", s.test)
 
-		resp, err := json.Marshal(initkeyboard)
-		if err != nil {
-			log.Println(errors.Wrap(err, "failed to marshal 'keyboard'"))
+	rou.HandleFunc("/keyboard", s.keyboardInit).Methods("GET")
+	rou.HandleFunc("/message", s.messageSet).Methods("POST")
+	rou.HandleFunc("/friend", s.newFriend).Methods("POST")
+	rou.HandleFunc("/friend/", s.removeFriend).Methods("DELETE")
+	rou.HandleFunc("/chat_room/", s.leaveChatRoom).Methods("DELETE")
+	err := rou.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		pathTemplate, err := route.GetPathTemplate()
+		if err == nil {
+			log.Println(errors.Wrap(err, "Path regexp:"), pathTemplate)
 		}
-		w.Write([]byte(string(resp) + "\n"))
-		return
+		queriesTemplates, err := route.GetQueriesTemplates()
+		if err == nil {
+			log.Println(errors.Wrap(err, "Queries templates: "), queriesTemplates)
+		}
+		queriesRegexps, err := route.GetQueriesRegexp()
+		if err == nil {
+			log.Println(errors.Wrap(err, "Queries regexps: "), queriesRegexps)
+		}
+		methods, err := route.GetMethods()
+		if err == nil {
+			log.Println(errors.Wrap(err, "Methods: "), methods)
+		}
+		log.Println()
+		return nil
+	})
+	if err != nil {
+		log.Println(errors.Wrap(err, "ERROR: "))
 	}
 
-	// curl -XPOST 'https://:your_server_url/message' -d '{
-	//   "user_key": "encryptedUserKey",
-	//   "type": "text",
-	//   "content": "차량번호등록"
-	// }'
-	if r.Method == "POST" && r.URL.Path == "/message" {
-		body, err := ioutil.ReadAll(r.Body)
-		r.Body.Close()
-		if err != nil {
-			log.Println(errors.Wrap(err, "failed to read body of /message"))
-		}
-		log.Printf("body: %s\n", string(body))
+	// http.Handle("/", rou)
+	// return rou
+}
 
-		// input unmarshaled body at message
-		var msg message
-		if err := json.Unmarshal(body, &msg); err != nil {
-			log.Println(errors.Wrap(err, "failed to unmarshal /message"))
-		}
+func (s *Server) test(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("TESTING...\n"))
+	return
+}
 
-		msgCon := messageKey(msg.Content)
-		// if there is no msgCon, return same msg
-		if msgCon == "none" {
-			msgCon = msg.Content
-		}
+// curl -XGET 'https://:your_server_url/keyboard'
+func (s *Server) keyboardInit(w http.ResponseWriter, r *http.Request) {
+	// CASE1 Type: 'text'
+	initkeyboard := keyboard{Type: "text"}
 
-		remsg := s.msgFor(msgCon)
-		resp, err := json.Marshal(response{
-			Message: respText{
-				Text: remsg}})
-		if err != nil {
-			log.Println(errors.Wrap(err, "failed to marshal response"))
-		}
-		log.Printf("send %s\n", string(resp))
-		w.Write([]byte(string(resp) + "\n"))
-		return
+	// // CASE2 Type: 'buttons'
+	// initkeyboard := make([]keyboard, 1)
+	// initkeyboard[0].Type = "buttons"
+	// initkeyboard[0].Buttons = []string{"option1", "option2", "option3"}
+
+	resp, err := json.Marshal(initkeyboard)
+	if err != nil {
+		log.Println(errors.Wrap(err, "failed to marshal 'keyboard'"))
+	}
+	w.Write([]byte(string(resp) + "\n"))
+	return
+}
+
+// curl -XPOST 'https://:your_server_url/message' -d '{
+//   "user_key": "encryptedUserKey",
+//   "type": "text",
+//   "content": "차량번호등록"
+// }'
+func (s *Server) messageSet(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+	if err != nil {
+		log.Println(errors.Wrap(err, "failed to read body of /message"))
+	}
+	log.Printf("body: %s\n", string(body))
+
+	// input unmarshaled body at message
+	var msg message
+	if err := json.Unmarshal(body, &msg); err != nil {
+		log.Println(errors.Wrap(err, "failed to unmarshal /message"))
 	}
 
-	// friend information
-	// curl -XPOST 'https://:your_server_url/friend' -d '{"user_key" : "HASHED_USER_KEY" }'
-	if r.Method == "POST" && r.URL.Path == "/friend" {
-		body, err := ioutil.ReadAll(r.Body)
-		r.Body.Close()
-		if err != nil {
-			log.Println(errors.Wrap(err, "failed to read body of /friend"))
-		}
-		log.Printf("body: %s\n", string(body))
-
-		var usr user
-		if err := json.Unmarshal(body, &usr); err != nil {
-			log.Println(errors.Wrap(err, "failed to unmarshal body of /friend"))
-		}
-		log.Printf("Friend %s joined\n", usr.UserKey)
-		return
+	msgCon := messageKey(msg.Content)
+	// if there is no msgCon, return same msg
+	if msgCon == "none" {
+		msgCon = msg.Content
 	}
 
-	if r.Method == "DELETE" {
-		split := strings.Split(r.URL.Path, ":")
-
-		// chatroom delete by admin
-		// curl -XDELETE 'https://:your_server_url/chat_room/HASHED_USER_KEY'
-		if split[0] == "/friend/" {
-			log.Printf("user %s deleted", split[1])
-		}
-
-		// chatroom deleted by user
-		// DELETE	http://:your_server_url/chat_room/:user_key
-		if split[0] == "/chat_room/" {
-			log.Printf("user %s leaved", split[2])
-		}
-		return
+	remsg := s.msgFor(msgCon)
+	resp, err := json.Marshal(response{
+		Message: respText{
+			Text: remsg}})
+	if err != nil {
+		log.Println(errors.Wrap(err, "failed to marshal response"))
 	}
+	log.Printf("send %s\n", string(resp))
+	w.Write([]byte(string(resp) + "\n"))
+	return
+}
+
+// friend information
+// curl -XPOST 'https://:your_server_url/friend' -d '{"user_key" : "HASHED_USER_KEY" }'
+func (s *Server) newFriend(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+	if err != nil {
+		log.Println(errors.Wrap(err, "failed to read body of /friend"))
+	}
+	log.Printf("body: %s\n", string(body))
+
+	var usr user
+	if err := json.Unmarshal(body, &usr); err != nil {
+		log.Println(errors.Wrap(err, "failed to unmarshal body of /friend"))
+	}
+	log.Printf("Friend %s joined\n", usr.UserKey)
+	return
+}
+
+func (s *Server) removeFriend(w http.ResponseWriter, r *http.Request) {
+	split := strings.Split(r.URL.Path, ":")
+
+	// chatroom delete by admin
+	// curl -XDELETE 'https://:your_server_url/chat_room/HASHED_USER_KEY'
+	if split[0] == "/friend/" {
+		log.Printf("user %s deleted", split[1])
+	}
+}
+
+func (s *Server) leaveChatRoom(w http.ResponseWriter, r *http.Request) {
+	split := strings.Split(r.URL.Path, ":")
+
+	// chatroom deleted by user
+	// DELETE	http://:your_server_url/chat_room/:user_key
+	if split[0] == "/chat_room/" {
+		log.Printf("user %s leaved", split[2])
+	}
+	return
 }
 
 var messageKeyMap = map[string]string{
@@ -187,6 +234,7 @@ func messageKey(rawmessage string) string {
 var menuURLMap = []string{"http://www.hanyang.ac.kr/web/www/-254", "http://www.hanyang.ac.kr/web/www/-255", "http://www.hanyang.ac.kr/web/www/-258", "http://www.hanyang.ac.kr/web/www/-256"}
 
 func (s *Server) msgFor(request string) string {
+	// var s *Server
 	if request == "status" {
 		c := status.ServerCheck()
 
