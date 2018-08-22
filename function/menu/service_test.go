7aeea4362aefce5df99769b28631145373f94b05
yoonsue <suejoe1996@gmail.com>
2018-08-22 23:04:22 +0900

    function: add some tests for function

--- function/menu/service_test.go
+++ function/menu/service_test.go
@@ -4,13 +4,17 @@ import (
 	"testing"
 
 	"github.com/PuerkitoBio/goquery"
+	model "github.com/yoonsue/labchat/model/menu"
+	"github.com/yoonsue/labchat/repository/inmem"
 )
 
 func TestGetSchool(t *testing.T) {
+	r := inmem.NewMenuRepository()
+	s := NewService(r)
 	// var s *service
 	testCases := []struct {
 		url          string
-		expectedRest string
+		expectedRest model.Restaurant
 	}{
 		{
 			"http://www.hanyang.ac.kr/web/www/-254",
@@ -27,10 +31,10 @@ func TestGetSchool(t *testing.T) {
 	}
 	for _, c := range testCases {
 		///// TO BE CHANGED : scrapMenu --> GetSchool
-		gotMenuRest, _ := scrapMenu(c.url)
+		gotMenu := s.GetSchool(c.url)
 
-		if c.expectedRest != gotMenuRest {
-			t.Errorf("expected %s, got %s", c.expectedRest, gotMenuRest)
+		if c.expectedRest != gotMenu.Restaurant {
+			t.Errorf("expected %s, got %s", c.expectedRest, gotMenu.Restaurant)
 		}
 	}
 }
@@ -55,5 +59,19 @@ func TestScrapMenu(t *testing.T) {
 		if err != nil {
 			t.Errorf("failed to get URL")
 		}
+	}
+}
+
+func TestNewService(t *testing.T) {
+	r := inmem.NewMenuRepository()
+	s := &service{
+		menus: r,
+	}
+
+	gotService := NewService(r)
+
+	tmpURL := "http://www.hanyang.ac.kr/web/www/-254"
+	if s.GetSchool(tmpURL).Restaurant != gotService.GetSchool(tmpURL).Restaurant {
+		t.Errorf("expected %s, got %s", s.GetSchool(tmpURL).Restaurant, gotService.GetSchool(tmpURL).Restaurant)
 	}
 }
--- function/status/server-check_test.go
+++ function/status/server-check_test.go
@@ -1,5 +1,24 @@
 package status
 
+import (
+	"testing"
+
+	model "github.com/yoonsue/labchat/model/status"
+)
+
+func TestServerCheck(t *testing.T) {
+	s := &model.Server{
+		Temperature: 100,
+	}
+
+	gotS := model.NewServer()
+	gotS.Temperature = 100
+
+	if s.Temperature != gotS.Temperature {
+		t.Errorf("expected %s, got %s", s.Temperature, gotS.Temperature)
+	}
+}
+
 // Cant get access in code coverage /sys.
 // func TestGetTemp(t *testing.T) {
 
