6a11f0045699566749d9f4130dedf37e73b4dc7b
yoonsue <suejoe1996@gmail.com>
2019-03-08 15:21:03 +0900

    function: change the server from CentOS to Ubuntu

--- function/status/server-check.go
+++ function/status/server-check.go
@@ -1,9 +1,8 @@
 package status
 
 import (
+	"io/ioutil"
 	"log"
-	"os/exec"
-	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -37,60 +36,25 @@ func (s *service) ServerCheck() model.Server {
 	return s.statusServer
 }
 
-// // Got information here :https://www.kernel.org/doc/Documentation/thermal/sysfs-api.txt
-// const temperatureFile = "/sys/class/thermal/thermal_zone0/temp"
+////// Ubuntu
+// Got information here :https://www.kernel.org/doc/Documentation/thermal/sysfs-api.txt
+const temperatureFile = "/sys/class/thermal/thermal_zone0/temp"
 
-// // getTemp returns server temperature
-// func getTemp() model.Temperature {
-// 	data, err := ioutil.ReadFile(temperatureFile)
-// 	if err != nil {
-// 		log.Println(errors.Wrap(err, "failed to read temperature file"))
-// 		return -1
-// 	}
-
-// 	// TO BE IMPLEMENTED : HOW TO DEAL WITH 'linebreak'
-// 	fileAsString := string(data)
-// 	fileLines := strings.Split(fileAsString, "\n")
-// 	fileAsInt, _ := strconv.Atoi(fileLines[0])
-
-// 	// Unit: millidegree Celsius
-// 	temp := model.Temperature(fileAsInt / 1000)
-
-// 	if temp <= 0 {
-// 		log.Printf("temperature is lower than zero: %s\n", temp.String())
-// 		return -1
-// 	}
-// 	return temp
-// }
-
-// ExampleCmdOutput return output of terminal command
-func exampleCmdOutput(cmd string) ([]byte, error) {
-	out, err := exec.Command("bash", "-c", cmd).Output()
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-	return out, nil
-}
-
-// getTemp returns server temperature CentOS
+// getTemp returns server temperature
 func getTemp() model.Temperature {
-	data, err := exampleCmdOutput("sensors | grep \"Core 0\"")
+	data, err := ioutil.ReadFile(temperatureFile)
 	if err != nil {
-		log.Println(errors.Wrap(err, "failed to execute 'sensors' command"))
+		log.Println(errors.Wrap(err, "failed to read temperature file"))
 		return -1
 	}
 
 	// TO BE IMPLEMENTED : HOW TO DEAL WITH 'linebreak'
-	byteToString := string(data)
-	fileLines := strings.Split(byteToString, "\n")
-
-	re := regexp.MustCompile("[0-9]+")
-	linestring := re.FindAllString(fileLines[0], -1)
-	tempInt, _ := strconv.Atoi(linestring[1])
+	fileAsString := string(data)
+	fileLines := strings.Split(fileAsString, "\n")
+	fileAsInt, _ := strconv.Atoi(fileLines[0])
 
 	// Unit: millidegree Celsius
-	temp := model.Temperature(tempInt)
+	temp := model.Temperature(fileAsInt / 1000)
 
 	if temp <= 0 {
 		log.Printf("temperature is lower than zero: %s\n", temp.String())
@@ -98,6 +62,43 @@ func getTemp() model.Temperature {
 	}
 	return temp
 }
+
+////// CentOS
+// // ExampleCmdOutput return output of terminal command
+// func exampleCmdOutput(cmd string) ([]byte, error) {
+// 	out, err := exec.Command("bash", "-c", cmd).Output()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 		return nil, err
+// 	}
+// 	return out, nil
+// }
+
+// // getTemp returns server temperature CentOS
+// func getTemp() model.Temperature {
+// 	data, err := exampleCmdOutput("sensors | grep \"Core 0\"")
+// 	if err != nil {
+// 		log.Println(errors.Wrap(err, "failed to execute 'sensors' command"))
+// 		return -1
+// 	}
+
+// 	// TO BE IMPLEMENTED : HOW TO DEAL WITH 'linebreak'
+// 	byteToString := string(data)
+// 	fileLines := strings.Split(byteToString, "\n")
+
+// 	re := regexp.MustCompile("[0-9]+")
+// 	linestring := re.FindAllString(fileLines[0], -1)
+// 	tempInt, _ := strconv.Atoi(linestring[1])
+
+// 	// Unit: millidegree Celsius
+// 	temp := model.Temperature(tempInt)
+
+// 	if temp <= 0 {
+// 		log.Printf("temperature is lower than zero: %s\n", temp.String())
+// 		return -1
+// 	}
+// 	return temp
+// }
 
 func getUptime(bootTime time.Time) time.Duration {
 	since := time.Since(bootTime)
